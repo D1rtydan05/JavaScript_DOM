535918b6208db774cc26f0c3473aca954b975788
Daniel Garcia <dgarcia300@west-mec.org>
2020-09-23 16:26:27 -0700

    yes

--- photos.js
+++ photos.js
@@ -2,8 +2,8 @@
 
  *    Photo gallery
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author: Daniel Garcia
+ *    Date:   9-8-20
 
  *    Filename: photos.js
  */
@@ -15,6 +15,7 @@ var photoOrder = [1,2,3,4,5];
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
+   alert("right arrow clicked");
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] + 1) === 6) {
          photoOrder[i] = 1;
@@ -27,6 +28,7 @@ function rightArrow() {
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
+   alert("left arrow clicked");
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] - 1) === 0) {
          photoOrder[i] = 5;
@@ -53,4 +55,15 @@ if (window.addEventListener) {
   window.addEventListener("load", setUpPage, false); 
 } else if (window.attachEvent)  {
   window.attachEvent("onload", setUpPage);
+}
+
+function createEventListeners(){
+   var leftArrow = document.getElementById('leftArrow');
+   alert(leftArrow);
+   var rightArrow = document.getElementById('rightArrow');
+   if (rightArrow.addEventListener){
+      rightArrow.addEventListener("click", rightArrow, false);
+   } else if (rightArrow.attachEvent){
+      rightArrow.attachEvent("onClick", rightArrow);
+   }
 }
